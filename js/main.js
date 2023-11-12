8f6896de98b8c3232369a3ca14a59e8ed9be468e
Alibek <52044412+Alibek15@users.noreply.github.com>
2023-11-12 16:35:18 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 document.addEventListener('DOMContentLoaded', async function() {
     const key = localStorage.getItem('key');
+    const isIndexPage = window.location.pathname.includes('index.html');
     console.log(key);
 
     if(key){
         const user = await axios.get(`https://justdb-fdea4-default-rtdb.europe-west1.firebasedatabase.app/data/${key}.json`)
         const userData = user.data;
+        if(isIndexPage){
+        const homeLink = document.querySelector('#homeLink');
+        homeLink.innerHTML=`<a class="nav-link" href="user.html">${userData.userName}</a>`
+        }
 
         const loginLink = document.querySelector('#loginLink');
         loginLink.innerHTML=`<a class="nav-link" href="../user.html">${userData.userName}</a>`
